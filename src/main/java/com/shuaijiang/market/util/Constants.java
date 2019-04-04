5a8db3b0c10b5368f6af6c539040e6771cecad98
shuaijiang5 <49082836+shuaijiang5@users.noreply.github.com>
2019-04-04 10:42:21 +0800

    Update Constants.java

--- src/main/java/com/shuaijiang/market/util/Constants.java
+++ src/main/java/com/shuaijiang/market/util/Constants.java
@@ -13,4 +13,18 @@ public class Constants {
          */
         public final static Integer PAGE_SIZE = 3;
     }
+    
+    public final static class path{
+        public final static String PATH_IMAGES = "D:\\Hasee\\IdeaProjects1808\\tiny\\src\\main\\webapp\\images\\";
+    }
+    public static String photos(MultipartFile photo){
+        String s = UUID.randomUUID().toString().replaceAll("-", "");
+        try {
+            photo.transferTo(new File(PATH_IMAGES + s));
+            return s;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
